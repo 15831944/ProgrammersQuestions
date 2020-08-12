50e04ff12d700c279f9b2f6fcbb6b4060fe0bca6
qujen <jenku2049@gmail.com>
2020-08-13 02:23:14 +0900

    indent

--- GetMissingNumber/GetMissingNumber.cs
+++ GetMissingNumber/GetMissingNumber.cs
@@ -2,19 +2,19 @@ sing System;
 
 class GFG
 {
-	    static int getMissingNumber(int[] a, int n)
-		        {
-				        int total = (n + 1) * (n + 2) / 2;
-					        for (int i = 0; i < n; i++)
-							            total -= a[i];
+	static int getMissingNumber(int[] a, int n)
+	{
+		int total = (n + 1) * (n + 2) / 2;
+		for (int i = 0; i < n; i++)
+			total -= a[i];
 
-						        return total;
-							    }
-	        static void Main(string[] args)
-			    {
-				            int[] a = {  2, 3, 4, 5, 6 };
-					            int miss = getMissingNumber(a, 5);
-						            Console.Write($"missing number is {miss}");
-							        }
+		return total;
+	}
+	static void Main(string[] args)
+	{
+		int[] a = {  2, 3, 4, 5, 6 };
+		int miss = getMissingNumber(a, 5);
+		Console.Write($"missing number is {miss}");
+	}
 }
 
